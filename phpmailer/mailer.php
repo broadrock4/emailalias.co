5fff13e501afe5546e2ecceb89c94d6a6a13747d
broadrock4 <burdi009@cougars.csusm.edu>
2020-11-16 15:51:19 -0800

    Commit 3

---
+++ phpmailer/mailer.php
@@ -0,0 +1,79 @@
+
+<?php
+
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// If necessary, modify the path in the require statement below to refer to the
+// location of your Composer autoload.php file.
+require("C:/xammp2/htdocs/CS441/phpmailer/vendor/autoload.php");
+
+// Replace sender@example.com with your "From" address.
+// This address must be verified with Amazon SES.
+$sender = 'tyburdick4@gmail.com';
+$senderName = 'Tyler Burdick';
+
+// Replace recipient@example.com with a "To" address. If your account
+// is still in the sandbox, this address must be verified.
+$recipient = 'burdi009@cougars.csusm.edu';
+
+// Replace smtp_username with your Amazon SES SMTP user name.
+$usernameSmtp = 'AKIA5SL74TMRGVYINPN4';
+
+// Replace smtp_password with your Amazon SES SMTP password.
+$passwordSmtp = 'BJue2LyqV8sAcJTa+nMHeoNpO7H70DtgmMdMrYVPlEy+';
+
+$host = 'email-smtp.us-east-2.amazonaws.com';
+$port = 587;
+
+// The subject line of the email
+$subject = 'Amazon SES test (SMTP interface accessed using PHP)';
+
+// The plain-text body of the email
+$bodyText =  "Email Test\r\nThis email was sent through the
+    Amazon SES SMTP interface using the PHPMailer class.";
+
+// The HTML-formatted body of the email
+$bodyHtml = '<h1>Email Test</h1>
+    <p>This email was sent through the
+    <a href="https://aws.amazon.com/ses">Amazon SES</a> SMTP
+    interface using the <a href="https://github.com/PHPMailer/PHPMailer">
+    PHPMailer</a> class.</p>';
+
+$mail = new PHPMailer(true);
+
+try {
+    // Specify the SMTP settings.
+    $mail->isSMTP();
+    $mail->setFrom($sender, $senderName);
+    $mail->Username   = $usernameSmtp;
+    $mail->Password   = $passwordSmtp;
+    $mail->Host       = $host;
+    $mail->Port       = $port;
+    $mail->SMTPAuth   = true;
+    $mail->SMTPSecure = 'tls';
+    $mail->addCustomHeader('X-SES-CONFIGURATION-SET');
+
+    // Specify the message recipients.
+    $mail->addAddress($recipient);
+    // You can also add CC, BCC, and additional To recipients here.
+
+    // Specify the content of the message.
+    $mail->isHTML(true);
+    $mail->Subject    = $subject;
+    $mail->Body       = $bodyHtml;
+    $mail->AltBody    = $bodyText;
+    $mail->Send();
+    echo "Email sent!" , PHP_EOL;
+} catch (phpmailerException $e) {
+    echo "An error occurred. {$e->errorMessage()}", PHP_EOL; //Catch errors from PHPMailer.
+} catch (Exception $e) {
+    echo "Email not sent. {$mail->ErrorInfo}", PHP_EOL; //Catch errors from Amazon SES.
+}
+
+
+
+
+?>
---
+++ phpmailer/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5ad53b18151d6e9cef0b297a5e8506b5229ab96a',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5ad53b18151d6e9cef0b297a5e8506b5229ab96a',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.1.8',
+      'version' => '6.1.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '917ab212fa00dc6eacbb26e8bc387ebe40993bc1',
+    ),
+  ),
+);
